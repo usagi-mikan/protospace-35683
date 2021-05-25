3f6d00acc2b4059924e25ce2e2311c94b34b11b2
usagi-mikan <makitakumiko228@gmail.com>
2021-05-25 19:07:23 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,62 @@
+class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:edit, :show]
+  before_action :move_to_edit, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+       redirect_to prototypes_index_path(@prototype)
+    else
+      render :index
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+       redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_edit
+    @prototype = Prototype.find(params[:id])
+
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+
+end
